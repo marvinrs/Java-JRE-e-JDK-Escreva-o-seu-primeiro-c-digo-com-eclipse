6dac078e9f26850eca4db73e6b623a4d33e22285
marvinrs <72471673+marvinrs@users.noreply.github.com>
2021-11-18 14:40:11 -0300

    Concatenando variaveis e String

---
+++ sintaxe-variaveis-e-fluxo/TestaVariaveis.java
@@ -0,0 +1,16 @@
+
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		
+		System.out.println("ola novo teste");
+
+		int idade;
+		idade = 37;
+		int soma = idade + 35;
+		
+		System.out.println(idade);
+		System.out.println("Sua idade é " + idade + ", parabéns!");
+		System.out.println(soma);
+	}
+
+}
