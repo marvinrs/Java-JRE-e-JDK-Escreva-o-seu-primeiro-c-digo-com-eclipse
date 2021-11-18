ed52ab7c0a413570f38ee00fd47151bbeb445f9d
marvinrs <72471673+marvinrs@users.noreply.github.com>
2021-11-18 14:43:49 -0300

    Testa condicional com booleano

---
+++ sintaxe-variaveis-e-fluxo/TestaCondicional2.java
@@ -0,0 +1,17 @@
+
+public class TestaCondicional2 {
+	
+	public static void main(String[] args) {
+		System.out.println("testando condicionais");
+		int idade = 16;
+		int quantidadePessoas = 2;
+		boolean acompanhado = quantidadePessoas >= 2;
+		if (idade >=18 && acompanhado) {
+			System.out.println("Você pode entrar.");
+		}	
+		 else {
+				System.out.println("infelizmente você não pode entrar.");
+			}
+	}
+
+}
