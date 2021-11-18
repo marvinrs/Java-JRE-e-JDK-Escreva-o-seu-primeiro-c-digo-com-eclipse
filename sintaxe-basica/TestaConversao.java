004c3c5e13878bdc7c57c8d7d5b58a804aa5e1b2
marvinrs <72471673+marvinrs@users.noreply.github.com>
2021-11-18 14:38:16 -0300

    Convertendo double em int

---
+++ sintaxe-basica/TestaConversao.java
@@ -0,0 +1,20 @@
+
+public class TestaConversao {
+	
+	public static void main (String[] args) {
+		
+		float pontoFlutuante = 3.14f;
+		
+		double salario = 1270.50;
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		double valor1 = 0.1;
+		double valor2 = 0.2;
+		double total = valor1 + valor2;
+		
+		System.out.println(pontoFlutuante);
+		
+	}
+
+}
